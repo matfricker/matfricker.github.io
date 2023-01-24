48d77fc9ebaeb9e382f7b6692cf1f8def88e6a98
Matt Fricker <matt@fricker.io>
2023-01-24 23:01:13 +0000

    add customer list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import logo from './logo.png';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 function App() {
 
-  function fetchCustomers() {
-    fetch("https://api.fricker.io/customers")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          console.error(error);
+  const [customer, setCustomer] = useState([]);
+
+  useEffect(() => {
+    customers()
+  }, []);
+
+  const customers = async () => {
+    const response = 
+      await fetch("https://api.fricker.io/customers", {
+        headers: {
+          'ClientApiKey': 'oXYG6Ub7ObGejWkbvCqwO7SlM6iZM3RewzwZ3i4gWlQK3zZyTPdoE4PUc9HQ'
         }
-      )
+      });
+
+    setCustomer(await response.json());
   }
 
   function start() {
-    let isStop = false;
     const logo = document.querySelector('.logo');
     // start logo animation
     if (logo.classList.contains('to-left')) {
@@ -30,40 +32,10 @@ function App() {
     }
   }
 
-  function switchDirection() {
-    const logo = document.querySelector('.logo');
-    // switch direction
-    if (logo.classList.contains('to-right')) {
-      spinLeft(logo);
-    } else {
-      spinRight(logo);
-    }
-  }
-
   function reset() {
     const logo = document.querySelector('.logo');
     // stop animation
     spinStop(logo);
-  }
-
-  function spinRight(element) {
-    if (!element.classList.contains('to-right')) {
-      element.classList.add('to-right');
-    }
-
-    if (element.classList.contains('to-left')) {
-      element.classList.remove('to-left');
-    }
-  }
-
-  function spinLeft(element) {
-    if (!element.classList.contains('to-left')) {
-      element.classList.add('to-left');
-    }
-
-    if (element.classList.contains('to-right')) {
-      element.classList.remove('to-right');
-    }
   }
 
   function spinStop(element) {
@@ -78,14 +50,21 @@ function App() {
 
   return (
     <div className="app">
-      <header className="header">
-        <p>Hello from React World</p>
+      <section className="content">
+        <ol>
+          {customer.map((data) => {
+            return (
+              <li>{data.firstName} {data.lastName}</li>
+            )
+          })}
+        </ol>
+      </section>
+
+      <section className="animation">
         <button id="btnStart" className="primary" onClick={start}>Start</button>
-        <button id="btnSwitch" className="primary" onClick={switchDirection}>Switch</button>
         <button id="btnReset" className="primary" onClick={reset}>Reset</button>
-        <button id="btnFetchCustomers" className="primary" onClick={fetchCustomers}>Fetch Customers</button>
         <img src={logo} className="logo" alt="logo" />
-      </header>
+      </section>
     </div>
   );
 
